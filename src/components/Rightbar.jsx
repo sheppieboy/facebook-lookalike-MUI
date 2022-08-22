f51b7823d6463280e972e797e23f3651b09e6488
Luke Sheppard <lukesheppard@gmail.com>
2022-08-22 19:44:11 -0400

    Added Rightbar component with Online Friends, Latest Conversations, and Latest Posts

---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,139 @@
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Divider,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const Rightbar = () => {
+  return (
+    <Box
+      sx={{
+        flex: 2,
+        p: 2,
+        display: { xs: "none", sm: "block" },
+      }}
+    >
+      <Box position="fixed">
+        <Typography variant="h6" sx={{ fontWeight: 100, mt: 1, mb: 2 }}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+          <Avatar alt="Remy Sharp" src="" />
+          <Avatar alt="Travis Howard" src="" />
+          <Avatar alt="Cindy Baker" src="" />
+          <Avatar alt="Agnes Walker" src="" />
+          <Avatar alt="Tever Henderson" src="" />
+          <Avatar alt="" src="" />
+          <Avatar alt="" src="" />
+          <Avatar alt="" src="" />
+          <Avatar alt="" src="" />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest Posts
+        </Typography>
+        <ImageList col={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+              src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
+              alt="Breakfast"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://images.unsplash.com/photo-1551782450-a2132b4ba21d"
+              alt="Burger and Fries"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c"
+              alt="Coffee"
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant="h6" sx={{ fontWeight: 100, mt: 2, mb: 2 }}>
+          Latest Conversations
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
+    </Box>
+  );
+};
+export default Rightbar;

3b259b66e588c35b5d9f81cc34083afbd3ca4ba8
Luke Sheppard <lukesheppard@gmail.com>
2022-08-22 17:50:14 -0400

    Added margin to Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,7 +13,7 @@ import {
 
 const Post = () => {
   return (
-    <Card>
+    <Card sx={{ margin: 5 }}>
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">

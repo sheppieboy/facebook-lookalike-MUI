acc178c20ee1396591adcc64497777d9820953b3
Luke Sheppard <lukesheppard@gmail.com>
2022-08-23 14:04:14 -0400

    Added an Add component that uses a modal and allows a user to add a Post

---
+++ src/components/Add.jsx
@@ -0,0 +1,102 @@
+import {
+  Fab,
+  IconButton,
+  Modal,
+  styled,
+  Tooltip,
+  Typography,
+  Box,
+  Avatar,
+  TextField,
+  Stack,
+  ButtonGroup,
+  Button,
+} from "@mui/material";
+import {
+  Add as AddIcon,
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from "@mui/icons-material";
+import { useState } from "react";
+
+const Add = () => {
+  const [open, setOpen] = useState(false);
+
+  const StyledModal = styled(Modal)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  });
+
+  const Userbox = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    marginBottom: "20px",
+  });
+  return (
+    <>
+      <Tooltip
+        title="Post"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% -25px)", md: 30 },
+        }}
+        onClick={() => setOpen(true)}
+      >
+        <IconButton>
+          <Fab color="primary" aria-label="add">
+            <AddIcon />
+          </Fab>
+        </IconButton>
+      </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={() => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography variant="h6" color="grey" textAlign="center">
+            Create Post
+          </Typography>
+          <Userbox>
+            <Avatar alt="" src="" sx={{ width: 30, height: 30 }} />
+            <Typography fontWeight={400} variant="span">
+              John doe
+            </Typography>
+          </Userbox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            variant="standard"
+            placeholder="What's on your mind?"
+            multiline
+            rows={4}
+          />
+          <Stack direction="row" gap={1} mt={2} mb={2}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="outlined primary button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "25%" }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
+    </>
+  );
+};
+export default Add;

083d6775fd3f1483d2625bde35d387ed1f6f3166
Luke Sheppard <lukesheppard@gmail.com>
2022-08-23 14:13:56 -0400

    Displayed elements in App.js and added dark mode and light mode theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import Feed from "./components/Feed";
+import Sidebar from "./components/Sidebar";
+import Rightbar from "./components/Rightbar";
+import { Box } from "@mui/system";
+import { createTheme, Stack, ThemeProvider } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+import { useState } from "react";
+
 function App() {
-  return <div>hello</div>;
+  const [mode, setMode] = useState("light");
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar mode={mode} setMode={setMode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;

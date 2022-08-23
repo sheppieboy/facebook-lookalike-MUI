6bc0b6e57f65e6a63f83bc6c36c2b0f15bede0c8
Luke Sheppard <lukesheppard@gmail.com>
2022-08-23 14:16:38 -0400

    fixed background color in the modal and Add Component

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -63,7 +63,7 @@ const Add = () => {
         <Box
           width={400}
           height={280}
-          bgcolor={"backgroundcolor.default"}
+          bgcolor={"background.default"}
           color={"text.primary"}
           p={3}
           borderRadius={5}
